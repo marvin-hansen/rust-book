9b172003c36cf77b5f404da3bbb13537abeb3cfa
Rust Academy <marvin.hansen@rust-academy.com>
2022-10-16 12:55:14 +0800

    Create fixed_heap_allocation.rs

---
+++ 2_Chapter/fixed_heap_allocation.rs
@@ -0,0 +1,12 @@
+// Section 2.5.1 Ownership
+fn main() {
+    let x = String::from("Hi");
+    let y = &x;
+    let z = x.clone();
+    println!("Value: {}", x);
+    println!("Reference: {}", y);
+    println!("Cloned value: {}", z);
+} // prints 
+// Value: Hi
+// Reference: Hi
+// Cloned value: Hi
