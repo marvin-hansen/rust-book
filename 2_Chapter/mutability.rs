0a9fef4ab4658aab617801d39512ab6af5b75376
Rust Academy <marvin.hansen@rust-academy.com>
2022-10-16 11:18:04 +0800

    Update mutability.rs

--- 2_Chapter/mutability.rs
+++ 2_Chapter/mutability.rs
@@ -5,6 +5,7 @@ fn main() {
  let x = 5;
  println!("The value of x is: {x}");
  // x = 6; // Throws: cannot assign twice to immutable variable
+ let x = 42; // shadows the previous x with a different value.
  let mut y = 5;
  println!("The value of y is: {y}");
  y = 6;
