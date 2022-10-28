60882db9e811a36c7a47499932616f49482d5c97
Rust Academy <marvin.hansen@rust-academy.com>
2022-10-28 19:33:28 +0800

    Create 2_14_utf_string.rs

---
+++ 2_Chapter/2_14_utf_string.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let arg = std::env::args().skip(1).next()
+        .expect("should have one argument");
+    println!("upp = {}", arg.to_uppercase());
+}
+// cargo run --quiet -- "dog" "cat" "parrot"
+// cargo run --quiet -- "🙈🙉🙊"
