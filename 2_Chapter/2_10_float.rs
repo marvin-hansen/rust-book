903173e19974e5f643d399375430ef30fb64a88c
Rust Academy <marvin.hansen@rust-academy.com>
2022-10-28 12:29:33 +0800

    Create 2_10_float.rs

---
+++ 2_Chapter/2_10_float.rs
@@ -0,0 +1,16 @@
+fn main() {    
+    let x = 2.0; // f64
+    let y: f32 = 3.0; // f32
+
+    // addition
+    let sum = x + y; //casts automatically to f64 
+
+    // subtraction
+    let difference = 95.5 - 4.3;
+
+    // multiplication
+    let product = y * x;
+
+    // division
+    let quotient = 56.7 / 32.2;
+}
