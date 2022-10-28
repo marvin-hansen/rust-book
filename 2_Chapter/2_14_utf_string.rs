ef4849a805505e598158719699cb06dece4f3da0
Rust Academy <marvin.hansen@rust-academy.com>
2022-10-28 20:17:08 +0800

    Update 2_14_utf_string.rs

--- 2_Chapter/2_14_utf_string.rs
+++ 2_Chapter/2_14_utf_string.rs
@@ -1,7 +1,28 @@
 fn main() {
-    let arg = std::env::args().skip(1).next()
-        .expect("should have one argument");
-    println!("upp = {}", arg.to_uppercase());
+    let hello = String::from("السلام عليكم");
+    print_something(&hello);
+    let hello = String::from("Dobrý den");
+    print_something(&hello);
+    let hello = String::from("Hello");
+    print_something(&hello);
+    let hello = String::from("שָׁלוֹם");
+    print_something(&hello);
+    let hello = String::from("नमस्ते");
+    print_something(&hello);
+    let hello = String::from("こんにちは");
+    print_something(&hello);
+    let hello = String::from("안녕하세요");
+    print_something(&hello);
+    let hello = String::from("你好");
+    print_something(&hello);
+    let hello = String::from("Olá");
+    print_something(&hello);
+    let hello = String::from("Здравствуйте");
+    print_something(&hello);
+    let hello = String::from("Hola");
+    print_something(&hello);
 }
-// cargo run --quiet -- "Приветствую, мир"
-// Prints: upp = ПРИВЕТСТВУЮ, МИР
+
+fn print_something(something: &str) {
+    println!("{}", something);
+}
